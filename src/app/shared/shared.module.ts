b4dc34358244372558f019929250ac998284a063
juano20614 <gojuan199@gmail.com>
2024-05-24 11:32:10 -0500

    agregado atomos,moleculas,organismos de header

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './components/atoms/home/home.component';
+import { LogoComponent } from './components/atoms/logo/logo.component';
+import { TituloComponent } from './components/atoms/titulo/titulo.component';
+import { HeaderComponent } from './components/organisms/header/header.component';
+import { PerfilComponent } from './components/molecules/perfil/perfil.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    LogoComponent,
+    TituloComponent,
+    HeaderComponent,
+    PerfilComponent
+  ],
   imports: [
     CommonModule
+  ],
+
+  exports:[
+    HeaderComponent
   ]
 })
 export class SharedModule { }
