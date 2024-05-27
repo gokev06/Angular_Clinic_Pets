484880fa07a3e81443c6cc7d37e660d34ca93c63
juano20614 <gojuan199@gmail.com>
2024-05-27 07:03:34 -0500

    agregado footer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,23 +5,37 @@ import { LogoComponent } from './components/atoms/logo/logo.component';
 import { TituloComponent } from './components/atoms/titulo/titulo.component';
 import { HeaderComponent } from './components/organisms/header/header.component';
 import { PerfilComponent } from './components/molecules/perfil/perfil.component';
+import { InfoFooterComponent } from './components/atoms/info-footer/info-footer.component';
+import { InconmapComponent } from './components/atoms/inconmap/inconmap.component';
+import { InconphoneComponent } from './components/atoms/inconphone/inconphone.component';
+import { InconmessageComponent } from './components/atoms/inconmessage/inconmessage.component';
+import { FooterComponent } from './components/organisms/footer/footer.component';
+import { ContactoInfoComponent } from './components/atoms/contacto-info/contacto-info.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
+    ContactoInfoComponent,
     LogoComponent,
     TituloComponent,
     HeaderComponent,
-    PerfilComponent
+    PerfilComponent,
+    InfoFooterComponent,
+    InconmapComponent,
+    InconphoneComponent,
+    InconmessageComponent,
+    FooterComponent,
+  
   ],
   imports: [
     CommonModule
   ],
 
   exports:[
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ]
 })
 export class SharedModule { }
