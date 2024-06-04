db10ca827d85c84b627023173308c81438c187dc
Gina Tamayo <ginatamayo1618@gmail.com>
2024-06-03 19:01:58 -0500

    creacion del modal de perfil

--- src/app/shared/components/molecules/perfil/perfil.component.ts
+++ src/app/shared/components/molecules/perfil/perfil.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
-  styleUrl: './perfil.component.scss'
+  styleUrls: ['./perfil.component.scss']
 })
 export class PerfilComponent {
+  isModalOpen = false;
 
+  toggleModal() {
+    this.isModalOpen = !this.isModalOpen;
+  }
+
+  editProfile() {
+    console.log('Editar perfil');
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
+  closeModalOnOutsideClick(event: Event) {
+    this.isModalOpen = false;
+  }
+
+  stopPropagation(event: Event) {
+    event.stopPropagation();
+  }
 }
