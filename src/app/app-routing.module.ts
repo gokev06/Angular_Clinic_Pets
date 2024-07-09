2083c00eea3e66be16b898487c415e4e6f6b1287
gokev06 <kevin2006gonzalez@gmail.com>
2024-07-09 10:17:46 -0500

    agregado pages de nueva publicacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { CitasComponent } from './features/citas/citas.component';
 import { PagehomeComponent } from './features/pagehome/pagehome.component';
 import { AdopcionComponent } from './features/adopcion/adopcion.component';
+import { NuevaadopcionComponent } from './features/adopcion/pages/nuevaadopcion/nuevaadopcion.component';
 
 const routes: Routes = [
   {path: "citas", component: CitasComponent},
   { path: "" , component: PagehomeComponent},
-  {path: "adopcion", component: AdopcionComponent}
+  {path: "adopcion", component: AdopcionComponent},
+  {path:"Nuevo-Publicacion" , component: NuevaadopcionComponent }
 ];
 
 @NgModule({
--- src/app/features/adopcion/adopcion.module.ts
+++ src/app/features/adopcion/adopcion.module.ts
@@ -8,13 +8,15 @@ import { HeaderadopcionComponent } from './components/headeradopcion/headeradopc
 import { SharedModule } from '../../shared/shared.module';
 import { CardsadopcionComponent } from './components/cardsadopcion/cardsadopcion.component';
 import { CardsComponent } from './components/cards/cards.component';
+import { NuevaadopcionComponent } from './pages/nuevaadopcion/nuevaadopcion.component';
 
 
 @NgModule({
   declarations: [  
     HeaderadopcionComponent,
      CardsadopcionComponent,
-     CardsComponent
+     CardsComponent,
+     NuevaadopcionComponent
   ],
   imports: [
     FormsModule,
