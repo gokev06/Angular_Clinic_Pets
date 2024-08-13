6de59d13d2c34e43041f12cea66a239759042fdd
juano20614 <jg5612162@gmail.com>
2024-08-13 15:11:55 -0500

    agregado formulario

--- src/app/features/tienda/components/agregar-producto/agregar-producto.component.ts
+++ src/app/features/tienda/components/agregar-producto/agregar-producto.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-agregar-producto',
@@ -9,17 +10,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class AgregarProductoComponent implements OnInit {
   @Output() datosProductoFormulario = new EventEmitter<any>();
 
-  estilos = {
-    padding: '10px',
-    backgroundColor: '#CCC4FF',
-    borderRadius: '5px',
-    width: '100%',
-    height: '40px',
-    fontSize: '15px',
-    border: 'none',
-    marginBottom: '10px',
-    color: 'black'
-  };
+  estilos = 'padding: 8px; background-color: #CCC4FF; border-radius: 5px; width: 100%; height:35px; font-size: 15px; border: none; margin-bottom: 5px; color: black';
   
   productoForm: FormGroup;
   selectedImage: string | ArrayBuffer | null = '';
@@ -32,7 +23,8 @@ export class AgregarProductoComponent implements OnInit {
     this.productoForm = this.formBuilder.group({
       nombre: ['', Validators.required],
       precio: ['', [Validators.required, Validators.min(1)]],
-      cantidad: ['', [Validators.required, Validators.min(1)]]
+      cantidad: ['', [Validators.required, Validators.min(1)]],
+      categoria:['', Validators.required]
     });
 
     this.productoForm.valueChanges.subscribe(valor => {
@@ -65,4 +57,29 @@ export class AgregarProductoComponent implements OnInit {
       console.log(this.productoForm.value);
     }
   }
+
+  AlertaAceptar() {
+    Swal.fire({
+      title: '¿Estas seguro de agregar este producto?',
+      imageUrl: 'assets/images/imgcitas/deciscion.png', 
+      imageWidth: 300,
+      imageHeight: 200,
+      confirmButtonText: 'Aceptar',
+      cancelButtonText: 'Cancelar',
+      showCancelButton: true, 
+      cancelButtonColor: 'rgba(209, 0, 0, 0.47)', 
+      confirmButtonColor: 'rgba(55, 163, 59, 0.47)'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: '¡Producto agregado!',
+          imageUrl: 'assets/images/imgcitas/confirmar.png',
+          imageWidth: 100,
+          imageHeight: 100,
+          imageAlt: 'Descripción de la imagen'
+        });
+      }
+    });
+  }
+  
 }
