e68bcfff9bf2f23d2b4e402bfbb39ac5de420760
federico722 <federicopelaezcalderon@gmail.com>
2024-09-03 21:30:35 -0500

    agrego movimiento de rutas a tienda, y boton agregar producto

--- src/app/features/tienda/components/header-tienda/header-tienda.component.ts
+++ src/app/features/tienda/components/header-tienda/header-tienda.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header-tienda',
@@ -6,11 +7,20 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./header-tienda.component.scss']
 })
 export class HeaderTiendaComponent {
+
+  constructor(private router: Router){}
+
+
+
    @Output() search = new EventEmitter<string>();
 
    onSearch(event: Event) {
     const searchTerm = (event.target as HTMLInputElement).value;
     this.search.emit(searchTerm);
+   }
+
+   redirectToAnotherPage(){
+    this.router.navigate(['/subir-producto'])
    }
 }
 
